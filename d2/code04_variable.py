af11ba30794a5c6156803aeb504cb47659b6ebd0
limchaeyeon8 <2000goal2001@gmail.com>
2023-01-31 10:38:25 +0900

    임시저장

---
+++ d2/code04_variable.py
@@ -0,0 +1,17 @@
+# 변수
+
+val = 'Hello'
+print(val)
+
+val = 3.1415926535897932384626433
+print(val)
+
+val = 10 // 2
+print(val)
+
+
+
+
+# 변수명 단어들의 조합
+cutie_ppaty_pretty_gorgie = 'me'
+print(cutie_ppaty_pretty_gorgie)
