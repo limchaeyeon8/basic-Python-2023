c7d823a6b0f5bd3664f6640ea6ab8d877436b5ab
limchaeyeon8 <2000goal2001@gmail.com>
2023-02-07 16:59:24 +0900

    230207

---
+++ d7/code41_debogging_on_jupyternotebook.py
@@ -0,0 +1,57 @@
+# %% [markdown]
+# # 주피터 노트북 사용법 학습
+# 
+# ## 마크다운, 파이썬 셀 추가
+# - 현재 셀 앞에 셀 추가 : a
+# - 현재 셀 뒤에 셀 추가 : b
+# - 현재 셀을 마크다운으로 변경  : 포커스(커서가 깜빡거리지 않는)만 있는 상태 m
+# - 현재 셀을 코드 셀로 변경 : 포커스 상태
+# 
+# 
+
+# %%
+# 최초 작성된 Python 셀
+
+# %% [markdown]
+# ## 파이썬 셀 실행
+# - 셀 실행 : Ctral + Enter
+# - 셀 실행 + 다음 셀 이동 : Shift + Enter        * 아래 쉘이 없으면 생성
+# - 셀 실행 + 다음 셀 생성 : Alt + Enter
+# - 주석처리 : Ctrl + / or Ctrl + k, c
+
+# %%
+print('Hello, Jupyter')
+
+# %% [markdown]
+# # 디버깅!!!
+# 아무리 강조해도 지나치지 않습니다
+# 
+# - 디버그 쉘 : Ctrl + Shift + Alt + Enter
+
+# %%
+arr = [1, '2', True, 'Hello', 3.1415926835897932383279]
+
+for i in arr:
+    print(i)
+
+# %% [markdown]
+# ## 함수 디버깅
+
+# %%
+def plus(x,y):
+    val = x + y
+    return val
+
+def div(x,y):
+    val = x / y
+    return val
+ 
+
+print('더하기')
+print(plus(10,4))
+
+
+# %%
+   
+print('나누기')
+print(div(10,0))
